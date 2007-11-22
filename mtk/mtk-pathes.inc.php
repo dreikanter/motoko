a19e627524ffbb5775e477265f77da12ffc841eb
Alex Musayev <alex.musayev@gmail.com>
2007-11-22 10:25:44 +0000

    Removed file/folder

--- mtk/mtk-pathes.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DIR_MTK', './mtk/');
-define('DIR_DATA', './mtk-data/');
-define('DIR_CONF', './mtk-conf/');
-define('DIR_PAGES', './mtk-pages/');
-define('DIR_LOGS', DIR_DATA.'logs/');
-
-?>
