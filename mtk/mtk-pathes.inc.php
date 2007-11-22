f360a9a512cc348a973536d1658fb5fc3871c950
Alex Musayev <alex.musayev@gmail.com>
2007-11-22 10:24:02 +0000

    Added a folder remotely

---
+++ mtk/mtk-pathes.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DIR_MTK', './mtk/');
+define('DIR_DATA', './mtk-data/');
+define('DIR_CONF', './mtk-conf/');
+define('DIR_PAGES', './mtk-pages/');
+define('DIR_LOGS', DIR_DATA.'logs/');
+
+?>
